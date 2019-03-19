1039c6fb62fa4a48b767c317e2c5583a1e33b146
victimoftrap <monsquizo@gmail.com>
2019-03-19 22:36:31 +0600

    Add links to pages in sidebar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from "react-router-dom";
 
 import './index.css';
 
 import Base from './layouts/base/Base';
 import Home from './pages/home/Home';
+import Done from './pages/done/Done';
 
 import './index.css';
 
 ReactDOM.render(
-    <Base>
-        <Home/>
-    </Base>,
+    <BrowserRouter>
+        <Base>
+            <Route exact path='/' component={Home}/>
+            <Route path='/done' component={Done}/>
+        </Base>
+    </BrowserRouter>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
--- src/layouts/base/components/sideBar/SideBar.js
+++ src/layouts/base/components/sideBar/SideBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {NavLink} from "react-router-dom";
+
 import './style.css';
 
 export default class SideBar extends React.Component {
@@ -9,14 +11,29 @@ export default class SideBar extends React.Component {
 
         return (
             <aside className={`side-bar${className ? ` ${className}` : ''}`}>
-                <ul className='side-bar__list'>
-                    <li className='side-bar__item item__todo'>
-                        <a className='side-bar__title' href='/'>To Do</a>
-                    </li>
-                    <li className='side-bar__item item__done'>
-                        <a className='side-bar__title' href='/'>Done</a>
-                    </li>
-                </ul>
+                <nav className={'side-bar__nav-menu nav-menu'}>
+                    <ul className={'nav-menu__list'}>
+                        <li className={'nav-menu__item'}>
+                            <NavLink
+                                to={'/'}
+                                exact
+                                className={'nav-menu__title item__todo'}
+                                activeClassName={'nav-menu__title_active item__todo_active'}
+                            >
+                                To Do
+                            </NavLink>
+                        </li>
+                        <li className={'nav-menu__item'}>
+                            <NavLink
+                                to={'/done'}
+                                className={'nav-menu__title item__done'}
+                                activeClassName={'nav-menu__title_active item__done_active'}
+                            >
+                                Done
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </aside>
         );
     };
