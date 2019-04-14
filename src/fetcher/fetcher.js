89f74bcf3b65b3ac8d4508bef53b2bb6952c8096
victimoftrap <monsquizo@gmail.com>
2019-04-15 02:45:04 +0600

    Added new fetchers

--- src/fetcher/fetcher.js
+++ src/fetcher/fetcher.js
@@ -10,21 +10,55 @@ function checkStatus(response) {
 }
 
 /**
+ * Build request to server
+ * @param requestMethod - http method name
+ * @param body - request body with data
+ * @returns request
+ */
+function buildRequest(requestMethod, body) {
+    return {
+        method: requestMethod,
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }),
+        body: body
+    };
+}
+
+/**
  * Get something from url
  * @param url url for making request
  */
 export function get(url) {
-    return fetch(url, {
-            method: 'GET',
-            headers: new Headers({
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            })
-        }
-    )
+    return fetch(url, buildRequest('GET', ''))
         .then(response => checkStatus(response))
         .then(response => response.json())
         .catch(error => {
             return error;
+        });
+}
+
+export function post(url, taskData) {
+    return fetch(url, buildRequest('POST', JSON.stringify(taskData)))
+        .then(response => checkStatus(response))
+        .catch(error => {
+            return error;
+        });
+}
+
+export function remove(url) {
+    return fetch(url, buildRequest('DELETE', ''))
+        .then(response => checkStatus(response))
+        .catch(error => {
+            return error
+        });
+}
+
+export function patch(url) {
+    return fetch(url, buildRequest('PATCH', ''))
+        .then(response => checkStatus(response))
+        .catch(error => {
+            return error
         });
 }
